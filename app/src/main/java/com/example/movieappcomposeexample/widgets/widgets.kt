a2a4ec66ae8e152b90987a8853e3a4b67be31e35
brahmadattagoli <g.brahmadatta@gmail.com>
2022-04-15 15:08:35 +0530

    loaded images using coil library

--- app/src/main/java/com/example/movieappcomposeexample/widgets/widgets.kt
+++ app/src/main/java/com/example/movieappcomposeexample/widgets/widgets.kt
@@ -1,24 +1,27 @@
 package com.example.movieappcomposeexample.widgets
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.Icon
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.rememberImagePainter
+import coil.transform.CircleCropTransformation
 import com.example.movieappcomposeexample.model.Movie
+import com.example.movieappcomposeexample.model.getMovies
 
+@Preview
 @Composable
-fun MovieRow(movie : Movie, onItemClick : (String) -> Unit = {}){
+fun MovieRow(movie : Movie = getMovies()[0], onItemClick : (String) -> Unit = {}){
 
     Card(modifier = Modifier
         .fillMaxWidth()
@@ -37,9 +40,17 @@ fun MovieRow(movie : Movie, onItemClick : (String) -> Unit = {}){
                 elevation = 5.dp,
                 shape = RectangleShape
             ) {
-                Icon(imageVector = Icons.Default.AccountBox, contentDescription = "Movie Image")
+                //Icon(imageVector = Icons.Default.AccountBox, contentDescription = "Movie Image")
+                Image(painter = rememberImagePainter(data = movie.images[0], builder = {
+                    crossfade(true)
+                    transformations(CircleCropTransformation())
+                }), contentDescription = "Movie Poster")
             }
-            Text(text = movie.title)
+            Column(modifier = Modifier.padding(5.dp)) {
+                Text(text = movie.title, style = MaterialTheme.typography.h6)
+                Text(text = "Director : ${movie.director}", style = MaterialTheme.typography.caption)
+                Text(text = "Released : ${movie.year}", style = MaterialTheme.typography.caption)
+            }
         }
     }
 
