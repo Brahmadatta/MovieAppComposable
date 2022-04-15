911b6dd710cc304953ff098de3d8eae99466adeb
brahmadattagoli <g.brahmadatta@gmail.com>
2022-04-15 15:28:20 +0530

    Added expand card functionality on click on the down arrow icon

--- app/src/main/java/com/example/movieappcomposeexample/widgets/widgets.kt
+++ app/src/main/java/com/example/movieappcomposeexample/widgets/widgets.kt
@@ -1,5 +1,7 @@
 package com.example.movieappcomposeexample.widgets
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
@@ -7,25 +9,36 @@ import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountBox
-import androidx.compose.runtime.Composable
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.rememberImagePainter
 import coil.transform.CircleCropTransformation
 import com.example.movieappcomposeexample.model.Movie
 import com.example.movieappcomposeexample.model.getMovies
 
+@OptIn(ExperimentalAnimationApi::class)
 @Preview
 @Composable
 fun MovieRow(movie : Movie = getMovies()[0], onItemClick : (String) -> Unit = {}){
 
+    var expanded by remember{
+        mutableStateOf(false)
+    }
     Card(modifier = Modifier
         .fillMaxWidth()
-        .height(130.dp)
+        //.height(130.dp)
         .clickable {
             onItemClick(movie.id)
         },
@@ -50,8 +63,37 @@ fun MovieRow(movie : Movie = getMovies()[0], onItemClick : (String) -> Unit = {}
                 Text(text = movie.title, style = MaterialTheme.typography.h6)
                 Text(text = "Director : ${movie.director}", style = MaterialTheme.typography.caption)
                 Text(text = "Released : ${movie.year}", style = MaterialTheme.typography.caption)
+                
+                AnimatedVisibility(visible = expanded) {
+                    Column() {
+                        Text(buildAnnotatedString {
+                            withStyle(style = SpanStyle(color = Color.DarkGray,
+                                                        fontSize = 13.sp)){
+                                append("Plot :")
+                            }
+                            withStyle(style = SpanStyle(color = Color.DarkGray,
+                                                        fontSize = 13.sp,
+                                                        fontWeight = FontWeight.Bold)){
+                                append(movie.plot)
+                            }
+
+                        })
+
+                    }
+                }
+                
+                Icon(imageVector = if (expanded) Icons.Filled.KeyboardArrowUp
+                    else Icons.Filled.KeyboardArrowDown, contentDescription = "Arrow Down",
+                    modifier = Modifier
+                        .size(25.dp)
+                        .clickable {
+                            expanded = !expanded
+                        },
+                    tint = Color.DarkGray)
             }
         }
+
+
     }
 
 }
